cc78dcfe2cb2b1c7eb18a107bf95bb609576be20
Jaycshen <schenxmu@gmail.com>
2018-12-10 13:15:37 +0800

    fix a bug

--- multiLabelSmooth.py
+++ multiLabelSmooth.py
@@ -47,7 +47,7 @@ def main():
             transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])
         ]),
     }
-    batch_size = 32
+    batch_size = 64
     image_datasets = {
             x: VisualConceptDataset(
                 os.path.join(data_dir, x),
@@ -64,9 +64,9 @@ def main():
             }
 
     dataset_sizes = {x: len(image_datasets[x]) for x in ['train', 'val']}
-    device = torch.device("cuda:2" if torch.cuda.is_available() else "cpu")
+    device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
     log_every = 10
-    print_every = 10 
+    print_every = 100 
     checkpoint_path = 'logs'
     if not os.path.exists(checkpoint_path):
         os.mkdir(checkpoint_path)
@@ -148,8 +148,12 @@ def main():
                 if phase == 'val' and epoch_acc > best_acc:
                     best_acc = epoch_acc
                     best_model_wts = copy.deepcopy(model.state_dict())
-                    torch.save(model.state_dict, os.path.join(checkpoint_path, 'model-best.pth'))
-                    torch.save(optimizer.state_dict, os.path.join(checkpoint_path, 'info-best.path'))
+                    torch.save(
+                            model.state_dict(), 
+                            os.path.join(checkpoint_path, 'model-best.pth'))
+                    torch.save(
+                            optimizer.state_dict(), 
+                            os.path.join(checkpoint_path, 'info-best.path'))
                     print("model save to %s/model-best.pth"%checkpoint_path)
 
             print()
@@ -175,16 +179,16 @@ def main():
     # todo : adapt to label smoothing
     criterion = nn.BCEWithLogitsLoss()
     # Observe that all parameters are being optimized
+    '''
     optimizer_ft = optim.SGD(
             model_ft.parameters(), 
-            lr=0.1, 
+            lr=0.01, 
             momentum=0.9)
     '''
     optimizer_ft = optim.Adam(
             model_ft.parameters(), 
-            lr = 0.00001
+            lr = 0.001
             )
-    '''
     # Decay LR by a factor of 0.1 every 7 epochs
     exp_lr_scheduler = lr_scheduler.StepLR(optimizer_ft, step_size=7, gamma=0.1)
 
@@ -196,7 +200,9 @@ def main():
         num_epochs=25
         )
 
-    torch.save(model.state_dict, os.path.join(checkpoint_path, 'model-best.pth'))
+    torch.save(
+            model_ft.state_dict(), 
+            os.path.join(checkpoint_path, 'model-best.pth'))
     print("model save to %s/model-best.pth"%checkpoint_path)
 
 if __name__ == '__main__':
